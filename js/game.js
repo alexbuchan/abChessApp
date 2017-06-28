7cf1a73f7d2aa63dbba7a2fe29ebdae12a453778
Alex Buchan <alex.buchan@telefonica.net>
2017-06-28 11:27:32 +0200

    Tiles know quantity of pieces. Pieces aware of which tile they're on.

--- js/game.js
+++ js/game.js
@@ -6,13 +6,14 @@
 //GOAL 4: No more than one piece should be present in a tile at once.
 //GOAL 5: Implement piece rules.
 
-
+//Game object.
 function Game() {
   this.boardSize = 8;                                                           //Board size (Not going to change obviously).
   this.generateTiles();                                                         //Generate game board with tiles.
                                                                                 //If you get a chance, when a player takes a piece, use skyrim level UP sound FX.
 }
 
+//Generates board squares.
 Game.prototype.generateTiles = function() {
   for (var row = 0; row < this.boardSize; row++) {
     $("#game-board").append($('<div>')
@@ -33,8 +34,7 @@ Game.prototype.addPieces = function (chessObjects) {
   for (var objectIndex in chessObjects) {
     var object = chessObjects[objectIndex];
     var piece = $('.tile[data-row=' + object.location.row + '][data-col=' + object.location.column + ']');
-    piece.append($('<img>').addClass("piece").attr("src", object.image)
-    );
+    piece.append($('<img>').addClass("piece").attr("src", object.image).attr("name", object.name));
     if (object.pieceType === "White") {
       piece.attr("pieceType", "white");
     }
@@ -46,20 +46,43 @@ Game.prototype.addPieces = function (chessObjects) {
 
 function movePieces(turn) {
   $(".piece").draggable({
-    snap: '.tile'
+    //snap: '.tile',
+    start: onClickInfo
   });
   $(".tile").droppable({
-    accept: function(element) {
-      return element.hasClass("acceptable");
-    }
-  });
-  $("a").bind("click", function(event) {
-    event.preventDefault();
-    $(".draggable").toggleClass("acceptable");
+    hoverClass : "hover",
+    drop : dropItemInfo,
   });
 }
 
 //Generic Functions
+// it's just too hard, give up....
+// Do a rock paper scissors console game-board
+//In a giant switch statement???
+// BUT OF COURSE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+function onClickInfo(event, ui) {
+  var xCoor = $(this).parent().attr("data-col");
+  var yCoor = $(this).parent().attr("data-row");
+  var tileLength = $(this).length;
+  console.log("Initial Position of", $(this).attr("name"), ": [", yCoor, ",", xCoor, "]");
+  //ui.helper.remove();
+}
+
+function dropItemInfo(event, ui) {
+  var chessPiece = ui.draggable;
+  var tile = $(this);
+  var tileRow = parseInt($(this).attr("data-row"));
+  var tileColumn = parseInt($(this).attr("data-col"));
+  console.log("tile:", tile);
+  console.log("Final Position of piece: [", tileRow, ",", tileColumn, "]");
+  // console.log(chessPiece.attr("name"), "has been received by tile", tileXCoor, tileYCoor);
+  $(this).append($(chessPiece).css({"top": 0, "left":0}));
+  _findTile(tileRow, tileColumn);
+  //.append($('<img>').addClass("piece").attr("src", object.image).attr("name", object.name));
+}
+
 
 function createCheckerboardDesign() {
   $(".row:odd .tile:odd").addClass("blackTile");                                //Creates the checkerboard style.
@@ -68,6 +91,17 @@ function createCheckerboardDesign() {
   $(".row:even .tile:odd").addClass("whiteTile");
 }
 
+
+function _findTile(row, col) {
+  var tile =  $(".tile[data-row=" + row + "][data-col=" + col + "]");
+  var tileLength =  $(".tile[data-row=" + row + "][data-col=" + col + "] > img").length;
+  console.log("Tile Object:", tile);
+  console.log("Pieces within tile [", row, ",", col, "] =>", tileLength);
+}
+
+function collisionDetect() {
+
+}
 
 var game;
 var run = true;
@@ -78,27 +112,17 @@ $(document).ready(function() {
   game = new Game();
   game.addPieces(chessObjects);
   movePieces(turn);
-  // $("html").on("dragover", function() {
-  //   $(this).addClass("dragging");
-  // });
-  // $("html").on("dragleave", function() {
-  //   $(this).removeClass("dragging");
-  // });
-  // $("html").on("drop", function(event){
-  //   event.preventDefault();
-  //   event.stopPropagation();
-  //   console.log("Dropped!");
-  // });
-
-
-  // while (run) {
-  //   if (whiteTurn === true) {
-  //     $("[pieceType=black]").droppable("disable");
-  //   }
-  //   if (blackTurn === true) {
-  //     $("[pieceType=white]").droppable("disable");
-  //   }
-  //   run = false;
-  // }
 
 });
+
+
+//Create piece
+//Piece is an object with various attributes
+//Each piece is draggable on chess board tiles.
+//Each piece should be aware of which tile they are on. (Necessary for legitimate moves by pieces)
+//Tile should keep a count of pieces it has on it.  (Necessary when taking an opponenets pieces)
+
+
+//Make a clone of the image when it lands on the tile.
+//This will surely be on top or under the original piece. Then just remove the original piece.
+//Have an "active" class representing dead or alive. If it is dead, whisk it to the side in your premade graveyards
--- js/pieces.js
+++ js/pieces.js
@@ -1,7 +1,8 @@
 
 //Ctrl + Cmd + G ---> Select all occurences of something
 
-function Piece(pieceType, location, image, rule) {
+function Piece(name, pieceType, location, image, rule) {
+    this.name = name;
     this.pieceType = pieceType;
     this.location = location;
     this.image = image;
@@ -10,15 +11,15 @@ function Piece(pieceType, location, image, rule) {
 
 
 
-function Pawn(pieceType, location, image, rule) {
-  Piece.call(this, pieceType, location, image, rule);
+function Pawn(name, pieceType, location, image, rule) {
+  Piece.call(this, name, pieceType, location, image, rule);
 
 }
 Pawn.prototype = Object.create(Piece.prototype);
 Pawn.prototype.constructor = Pawn;
 
-function Knight(pieceType, location, image, rule) {
-  Piece.call(this, pieceType, location, image, rule);
+function Knight(name, pieceType, location, image, rule) {
+  Piece.call(this, name, pieceType, location, image, rule);
 }
 Knight.prototype = Object.create(Piece.prototype);
 Knight.prototype.constructor = Knight;
@@ -28,26 +29,26 @@ Knight.prototype.constructor = Knight;
 //   movesArray = []
 // }
 
-function Bishop(pieceType, location, image, rule) {
-  Piece.call(this, pieceType, location, image, rule);
+function Bishop(name, pieceType, location, image, rule) {
+  Piece.call(this, name, pieceType, location, image, rule);
 }
 Bishop.prototype = Object.create(Piece.prototype);
 Bishop.prototype.constructor = Bishop;
 
-function Rook(pieceType, location, image, rule) {
-  Piece.call(this, pieceType, location, image, rule);
+function Rook(name, pieceType, location, image, rule) {
+  Piece.call(this, name, pieceType, location, image, rule);
 }
 Rook.prototype = Object.create(Piece.prototype);
 Rook.prototype.constructor = Rook;
 
-function Queen(pieceType, location, image, rule) {
-  Piece.call(this, pieceType, location, image, rule);
+function Queen(name, pieceType, location, image, rule) {
+  Piece.call(this, name, pieceType, location, image, rule);
 }
 Queen.prototype = Object.create(Piece.prototype);
 Queen.prototype.constructor = Queen;
 
-function King(pieceType, location, image, rule) {
-  Piece.call(this, pieceType, location, image, rule);
+function King(name, pieceType, location, image, rule) {
+  Piece.call(this, name, pieceType, location, image, rule);
 }
 King.prototype = Object.create(Piece.prototype);
 King.prototype.constructor = King;
@@ -55,48 +56,48 @@ King.prototype.constructor = King;
 var chessObjects = {
 //Kings
 
-white_king : new King("White", {row : 7, column : 4}, "./images/white-pieces/white-king.png", ""),
-black_king : new King("Black", {row : 0, column : 4}, "./images/black-pieces/black-king.png", ""),
+white_king : new King("White King", "White", {row : 7, column : 4}, "./images/white-pieces/white-king.png", ""),
+black_king : new King("Black King", "Black", {row : 0, column : 4}, "./images/black-pieces/black-king.png", ""),
 //Queens
 
-white_queen : new Queen("White", {row : 7, column : 3}, "./images/white-pieces/white-queen.png", ""),
-black_queen : new Queen("Black", {row : 0, column : 3}, "./images/black-pieces/black-queen.png", ""),
+white_queen : new Queen("White Queen", "White", {row : 7, column : 3}, "./images/white-pieces/white-queen.png", ""),
+black_queen : new Queen("Black Queen", "Black", {row : 0, column : 3}, "./images/black-pieces/black-queen.png", ""),
 //Rooks
 //Bishops
-white_KsRook : new Rook("White", {row : 7, column : 7}, "./images/white-pieces/white-rook.png", ""),
-white_QsRook : new Rook("White", {row : 7, column : 0}, "./images/white-pieces/white-rook.png", ""),
-black_KsRook : new Rook("Black", {row : 0, column : 0}, "./images/black-pieces/black-rook.png", ""),
-black_QsRook : new Rook("Black", {row : 0, column : 7}, "./images/black-pieces/black-rook.png", ""),
+white_KsRook : new Rook("White Kingside Rook", "White", {row : 7, column : 7}, "./images/white-pieces/white-rook.png", ""),
+white_QsRook : new Rook("White Queenside Rook", "White", {row : 7, column : 0}, "./images/white-pieces/white-rook.png", ""),
+black_KsRook : new Rook("Black Kingside Rook", "Black", {row : 0, column : 0}, "./images/black-pieces/black-rook.png", ""),
+black_QsRook : new Rook("Black Queenside Rook", "Black", {row : 0, column : 7}, "./images/black-pieces/black-rook.png", ""),
 
-white_LightBishop : new Bishop("White", {row : 7, column : 5}, "./images/white-pieces/white-bishop.png", ""),
-white_DarkBishop : new Bishop("White", {row : 7, column : 2}, "./images/white-pieces/white-bishop.png", ""),
-black_LightBishop : new Bishop("Black", {row : 0, column : 2}, "./images/black-pieces/black-bishop.png", ""),
-black_DarkBishop : new Bishop("Black", {row : 0, column : 5}, "./images/black-pieces/black-bishop.png", ""),
+white_LightBishop : new Bishop("White Lightsquare Bishop", "White", {row : 7, column : 5}, "./images/white-pieces/white-bishop.png", ""),
+white_DarkBishop : new Bishop("White Darksquare Bishop", "White", {row : 7, column : 2}, "./images/white-pieces/white-bishop.png", ""),
+black_LightBishop : new Bishop("Black Lightsquare Bishop", "Black", {row : 0, column : 2}, "./images/black-pieces/black-bishop.png", ""),
+black_DarkBishop : new Bishop("Black Darksquare Bishop", "Black", {row : 0, column : 5}, "./images/black-pieces/black-bishop.png", ""),
 //Knights
 
-white_KsKnight : new Knight("White", {row : 7, column : 6}, "./images/white-pieces/white-knight.png", ""),
-white_QsKnight : new Knight("White", {row : 7, column : 1}, "./images/white-pieces/white-knight.png", ""),
-black_KsKnight : new Knight("Black", {row : 0, column : 1}, "./images/black-pieces/black-knight.png", ""),
-black_QsKnight : new Knight("Black", {row : 0, column : 6}, "./images/black-pieces/black-knight.png", ""),
+white_KsKnight : new Knight("White Kingside Knight", "White", {row : 7, column : 6}, "./images/white-pieces/white-knight.png", ""),
+white_QsKnight : new Knight("White Queenside Knight", "White", {row : 7, column : 1}, "./images/white-pieces/white-knight.png", ""),
+black_KsKnight : new Knight("Black Kingside Knight", "Black", {row : 0, column : 1}, "./images/black-pieces/black-knight.png", ""),
+black_QsKnight : new Knight("Black Queenside Knight", "Black", {row : 0, column : 6}, "./images/black-pieces/black-knight.png", ""),
 //Pawns
 
-white_APawn : new Pawn("White", {row : 6, column : 0}, "./images/white-pieces/white-pawn.png", ""),
-white_BPawn : new Pawn("White", {row : 6, column : 1}, "./images/white-pieces/white-pawn.png", ""),
-white_CPawn : new Pawn("White", {row : 6, column : 2}, "./images/white-pieces/white-pawn.png", ""),
-white_DPawn : new Pawn("White", {row : 6, column : 3}, "./images/white-pieces/white-pawn.png", ""),
-white_EPawn : new Pawn("White", {row : 6, column : 4}, "./images/white-pieces/white-pawn.png", ""),
-white_FPawn : new Pawn("White", {row : 6, column : 5}, "./images/white-pieces/white-pawn.png", ""),
-white_GPawn : new Pawn("White", {row : 6, column : 6}, "./images/white-pieces/white-pawn.png", ""),
-white_HPawn : new Pawn("White", {row : 6, column : 7}, "./images/white-pieces/white-pawn.png", ""),
+white_APawn : new Pawn("White A Pawn", "White", {row : 6, column : 0}, "./images/white-pieces/white-pawn.png", ""),
+white_BPawn : new Pawn("White B Pawn", "White", {row : 6, column : 1}, "./images/white-pieces/white-pawn.png", ""),
+white_CPawn : new Pawn("White C Pawn", "White", {row : 6, column : 2}, "./images/white-pieces/white-pawn.png", ""),
+white_DPawn : new Pawn("White D Pawn", "White", {row : 6, column : 3}, "./images/white-pieces/white-pawn.png", ""),
+white_EPawn : new Pawn("White E Pawn", "White", {row : 6, column : 4}, "./images/white-pieces/white-pawn.png", ""),
+white_FPawn : new Pawn("White F Pawn", "White", {row : 6, column : 5}, "./images/white-pieces/white-pawn.png", ""),
+white_GPawn : new Pawn("White G Pawn", "White", {row : 6, column : 6}, "./images/white-pieces/white-pawn.png", ""),
+white_HPawn : new Pawn("White H Pawn", "White", {row : 6, column : 7}, "./images/white-pieces/white-pawn.png", ""),
 
-black_APawn : new Pawn("Black", {row : 1, column : 0}, "./images/black-pieces/black-pawn.png", ""),
-black_BPawn : new Pawn("Black", {row : 1, column : 1}, "./images/black-pieces/black-pawn.png", ""),
-black_CPawn : new Pawn("Black", {row : 1, column : 2}, "./images/black-pieces/black-pawn.png", ""),
-black_DPawn : new Pawn("Black", {row : 1, column : 3}, "./images/black-pieces/black-pawn.png", ""),
-black_EPawn : new Pawn("Black", {row : 1, column : 4}, "./images/black-pieces/black-pawn.png", ""),
-black_FPawn : new Pawn("Black", {row : 1, column : 5}, "./images/black-pieces/black-pawn.png", ""),
-black_GPawn : new Pawn("Black", {row : 1, column : 6}, "./images/black-pieces/black-pawn.png", ""),
-black_HPawn : new Pawn("Black", {row : 1, column : 7}, "./images/black-pieces/black-pawn.png", "")
+black_APawn : new Pawn("Black A Pawn", "Black", {row : 1, column : 0}, "./images/black-pieces/black-pawn.png", ""),
+black_BPawn : new Pawn("Black B Pawn", "Black", {row : 1, column : 1}, "./images/black-pieces/black-pawn.png", ""),
+black_CPawn : new Pawn("Black C Pawn", "Black", {row : 1, column : 2}, "./images/black-pieces/black-pawn.png", ""),
+black_DPawn : new Pawn("Black D Pawn", "Black", {row : 1, column : 3}, "./images/black-pieces/black-pawn.png", ""),
+black_EPawn : new Pawn("Black E Pawn", "Black", {row : 1, column : 4}, "./images/black-pieces/black-pawn.png", ""),
+black_FPawn : new Pawn("Black F Pawn", "Black", {row : 1, column : 5}, "./images/black-pieces/black-pawn.png", ""),
+black_GPawn : new Pawn("Black G Pawn", "Black", {row : 1, column : 6}, "./images/black-pieces/black-pawn.png", ""),
+black_HPawn : new Pawn("Black H Pawn", "Black", {row : 1, column : 7}, "./images/black-pieces/black-pawn.png", "")
 };
 
 
@@ -108,5 +109,5 @@ black_HPawn : new Pawn("Black", {row : 1, column : 7}, "./images/black-pieces/bl
 //   console.log(object.pieceType);
 // }
 
-//$('.tile[data-row=0][data-col=0]').append('<div class="piece"><img src="' + black_APawn.image + '"></div>');
-//$('.tile[data-row=0][data-col=0]').append('<div class="piece"><img src="' + black_APawn.image + '"></div>');
+//$('."", tile[data-row=0][data-col=0]').append('<div class="piece"><img src="' + black_APawn.image + '"></div>');
+//$('."", tile[data-row=0][data-col=0]').append('<div class="piece"><img src="' + black_APawn.image + '"></div>');
