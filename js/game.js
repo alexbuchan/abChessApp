0d4e990a722386f94e8c4b1c4497a5c18c2f21fa
Alex Buchan <alex.buchan@telefonica.net>
2017-06-29 09:25:29 +0200

    Added collision detection.

--- js/game.js
+++ js/game.js
@@ -2,14 +2,14 @@
 
 //GOAL 1: Get pieces to appear on the board in the right spots. DONE
 //GOAL 2: Make them draggable. DONE
-//GOAL 3: Draggable pieces must adhere to tiles. KINDA
-//GOAL 4: No more than one piece should be present in a tile at once.
+//GOAL 3: Draggable pieces must adhere to $tiles. KINDA
+//GOAL 4: No more than one piece should be present in a $tile at once.
 //GOAL 5: Implement piece rules.
 
 //Game object.
 function Game() {
   this.boardSize = 8;                                                           //Board size (Not going to change obviously).
-  this.generateTiles();                                                         //Generate game board with tiles.
+  this.generateTiles();                                                         //Generate game board with $tiles.
                                                                                 //If you get a chance, when a player takes a piece, use skyrim level UP sound FX.
 }
 
@@ -33,14 +33,14 @@ Game.prototype.generateTiles = function() {
 Game.prototype.addPieces = function (chessObjects) {
   for (var objectIndex in chessObjects) {
     var object = chessObjects[objectIndex];
-    var tile = $('.tile[data-row=' + object.location.row + '][data-col=' + object.location.column + ']');
-    tile.append($('<img>')
+    var $tile = $('.tile[data-row=' + object.location.row + '][data-col=' + object.location.column + ']');
+    $tile.append($('<img>')
       .addClass("piece")
       .attr("alive", true)
       .attr("name", object.name)
       .attr("src", object.image)
     );
-    tile.attr("hosting", true);
+    $tile.attr("hosting", true);
     if (object.color === "White") {
       $('.piece[name="' + object.name + '"]').attr("color", "white");
       //piece.attr("color", "white");
@@ -71,26 +71,26 @@ function movePieces(turn) {
 function onClickInfo(event, ui) {
   var xCoor = $(this).parent().attr("data-col");
   var yCoor = $(this).parent().attr("data-row");
-  var tile = $(this).parent();
+  var $tile = $(this).parent();
   console.log("Initial Position of", $(this).attr("name"), ": [", yCoor, ",", xCoor, "]");
-  console.log("tile", tile);
+  console.log("tile", $tile);
   console.log("End onClickInfo()");
 }
 
 function dropItemInfo(event, ui) {
-  var tile = $(this);
+  var $tile = $(this);
   var chessPiece = ui.draggable;
-  var tileRow = parseInt($(this).attr("data-row"));
-  var tileColumn = parseInt($(this).attr("data-col"));
-  console.log("tile:", tile);
-  console.log("Final Position of piece: [", tileRow, ",", tileColumn, "]");
-  console.log("original position tile", tile.originalPosition);
+  var $tileRow = parseInt($(this).attr("data-row"));
+  var $tileColumn = parseInt($(this).attr("data-col"));
+  console.log("$tile:", $tile);
+  console.log("Final Position of piece: [", $tileRow, ",", $tileColumn, "]");
+  console.log("original position $tile", $tile.originalPosition);
 
   // $(this).append($(chessPiece).css({"top": 0, "left":0}));
 
-  console.log("tile.length", tile.children().length);
-  if (tile.children().length < 1) {
-
+  console.log("tile.length", $tile.children().length);
+  if ($tile.children().length < 1) {
+    $(this).append($(chessPiece).css({"top": 0, "left":0}));
   }
   else {
     $(this).droppable({
@@ -98,10 +98,11 @@ function dropItemInfo(event, ui) {
           ui.draggable.draggable({"revert": true});
       }
     });
+    ui.draggable.css({position: "relative", top: "0px", left: "0px"});
   }
-  _findTile(tileRow, tileColumn);
-  collisionDetect(ui, event, tile);
-  // detectColor(tile);
+  _findTile($tileRow, $tileColumn);
+  collisionDetect(ui, event, $tile);
+  // detectColor($tile);
 }
 
 
@@ -114,41 +115,41 @@ function createCheckerboardDesign() {
 
 
 function _findTile(row, col) {
-  var tile =  $(".tile[data-row=" + row + "][data-col=" + col + "]");
-  var tileLength =  $(".tile[data-row=" + row + "][data-col=" + col + "] > img").length;
-  console.log("Tile Object:", tile);
-  console.log("Pieces within tile [", row, ",", col, "] =>", tileLength);
+  var $tile =  $(".tile[data-row=" + row + "][data-col=" + col + "]");
+  var $tileLength =  $(".tile[data-row=" + row + "][data-col=" + col + "] > img").length;
+  console.log("Tile Object:", $tile);
+  console.log("Pieces within tile [", row, ",", col, "] =>", $tileLength);
 }
 
-function collisionDetect(ui, event, tile) {
-  var sameColor = detectColor(tile);
+function collisionDetect(ui, event, $tile) {
+  var sameColor = detectColor($tile);
   console.log("sameColor", sameColor);
-  var childrenArray = tile.children();
+  var childrenArray = $tile.children();
   var victim = $(childrenArray[0]);
   var attacker = $(childrenArray[1]);
-  kill(ui, event, tile, victim);
-  // goAway(ui, event, tile, attacker);
+  kill(ui, event, $tile, victim);
+  // goAway(ui, event, $tile, attacker);
 }
 
-function kill(ui, event, tile, victim) {
-  var sameColor = detectColor(tile);
+function kill(ui, event, $tile, victim) {
+  var sameColor = detectColor($tile);
   if (!sameColor) {
     victim.remove();
     ui.draggable.draggable({revert: true});
   }
 }
 
-// function goAway(ui, event, tile, attacker) {
-//   var sameColor = detectColor(tile);
+// function goAway(ui, event, $tile, attacker) {
+//   var sameColor = detectColor($tile);
 //   if (sameColor) {
 //     ui.draggable.draggable({revert: "invalid"});
 //   }
 // }
 
 
-function detectColor(tile) {
+function detectColor($tile) {
   console.log("Start Function detectColor()");
-  var childrenArray = tile.children();
+  var childrenArray = $tile.children();
   if (childrenArray.length <= 1) { return "No detection needed."; }
   var victimColor = $(childrenArray[0]).attr("color");
   var attackerColor = $(childrenArray[1]).attr("color");
@@ -173,4 +174,4 @@ $(document).ready(function() {
 //COLLISION DETECTION AND DEATH
 //Piece color will influence whether collisionDetect will not allow it to move to the same div (and not be appended)
 //If piece is different colour then allow the move and then modify the class alive to false for
-//first image in tile.children() (element 0).
+//first image in $tile.children() (element 0).
